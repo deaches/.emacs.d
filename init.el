b49758fc9441e01dc028b568ce9eede2690b7b33
Dave Chesser <dave@deaches.net>
2025-05-05 07:08:55 -0400

    package: Install AUCTeX

--- init.el
+++ init.el
@@ -16,3 +16,16 @@
 
 (use-package magit
   :bind ("<f12>" . magit-status))
+
+;; Probably not going to fret about fixing up `TeX-open-quote' and
+;; `TeX-close-quote' for ConTeXt.  Rather than syntactical sugar,
+;; ConTeXt would rather us use \quotation{} and \quote{} for outer and
+;; inner quotes.  As of my dive in ConTeXt's souce, this is to utilize
+;; language-specific rules, such as curly quotes (“ and ”) in English.
+;;
+;; Related: https://wiki.contextgarden.net/Command/setuplanguage
+
+(use-package tex
+  :ensure auctex
+  :custom
+  (ConTeXt-Mark-version "IV" "For some reason AucTeX defaults to MkII"))
